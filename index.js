96b63fecc57e0cd9bd2aa2694def1254998632b5
Swadhin941 <swadhinghose@gmail.com>
2023-11-23 12:19:32 +0600

    Mongo initial

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express= require('express');
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const jwt= require('jsonwebtoken');
+const ACCESS_TOKEN= process.env.ACCESS_TOKEN;
 
 
 //Middlewares
@@ -9,10 +13,53 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wxzkvmx.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    const Users = client.db('miniSocial').collection('User');
+
+    try {
+        app.post('/user', async (req, res) => {
+            const userData = req.body;
+            const checkEmail = await Users.find({ email: userData }).toArray();
+            if (checkEmail.length === 0) {
+                const result = await Users.insertOne(userData);
+                return res.send(result);
+            }
+            else {
+                return res.send({ acknowledged: true });
+            }
+        });
+
+        app.post('/jwt', async(req, res)=>{
+            const email= req.body.email;
+            const token= jwt.sign({email}, ACCESS_TOKEN, {expiresIn: "1h"});
+            res.send({token});
+        })
+    }
+    finally {
+
+    }
+}
+run()
+    .catch(error => {
+        console.log(error.message);
+    })
+
+
+app.get('/', (req, res) => {
     res.send('appifylab root link');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`appifyLab port opening  ${port}`);
 })
